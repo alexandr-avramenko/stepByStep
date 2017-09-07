06ccfa6fdf00cbdb08ec4c9677e4f1e8a603cadd
Alex Avramenko <avramenko423@gmail.com>
2017-09-08 01:03:15 +0300

    update all

---
+++ massive/camelize.js
@@ -0,0 +1,24 @@
+var ask = 'background-color';
+// var arr = ask.split('-');
+//
+// alert(arr[0]);
+
+var result = convertToMassive(toUp(ask));
+console.log(result);
+
+function toUp(str) {
+    var arr = str.split("");
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] == "-") {
+                var x = arr[i + 1],
+                    y = x.toUpperCase();
+                arr[i + 1] = y;
+            }
+    }
+    return arr.join('');
+}
+
+function convertToMassive(str) {
+    var converted = str.split('-');
+    return converted.join("");
+}
