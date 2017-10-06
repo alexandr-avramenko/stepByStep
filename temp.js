fff0ce2178f9c89acf3cedf1c44ec25773e20305
Alex Avramenko <avramenko423@gmail.com>
2017-10-06 10:52:45 +0300

    update

--- temp.js
+++ temp.js
@@ -1,8 +1,8 @@
 var arr = [
-    [1, 2, 3],
+    1, 2, 3,
     [4],
     ['You', 'just', 'learn', 'javascript'],
-    [112, 143124,44124]
+    112, 143124,44124
 ];
 
 // changeToString(arr);
@@ -16,7 +16,22 @@ var arr = [
 //         return(result.join());
 // }
 
-var res = arr.reduce(function (n, current) {
-    return n + ',' + current.join()
-        }, []);
-console.log(res);
+// var result = temp.reduce(function (n, current) {
+//     return n + ',' + current.join()
+//         }, []);
+// console.log(result);
+
+var result = convertToArray(arr).join();
+console.log(result);
+
+function convertToArray(arr) {
+    var result = [],
+        i;
+    for (i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i]) == true) {
+            result.push(arr[i].join());
+        } else {
+            result.push(arr[i]);
+        }
+    } return result;
+}
