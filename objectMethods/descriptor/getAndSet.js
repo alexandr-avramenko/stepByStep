f3c13bb8283c6c6b72717fc3e7ad3bf770c0b594
Alexander Avramenko <alexander.avramenko@netent.com>
2017-12-06 18:23:07 +0200

    update

--- objectMethods/descriptor/getAndSet.js
+++ objectMethods/descriptor/getAndSet.js
@@ -4,12 +4,20 @@ function User(fullName) {
         Object.defineProperties(this, {
 
             firstName: {
+                get: function () {
+                  return this.firstName;
+                },
+
                 set: function (value) {
                     this.firstName = value.split(' ')[0];
                 }
             },
 
             lastName: {
+                get: function () {
+                    return this.firstName;
+                },
+
                 set: function(value) {
                     var arr = value.split(' ');
                     this.lastName = arr[1];
