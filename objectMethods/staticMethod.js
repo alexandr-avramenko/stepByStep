435026e15125900ce2c391898d3213f46e56e8a3
Alexander Avramenko <alexander.avramenko@netent.com>
2017-12-07 17:10:01 +0200

    update

---
+++ objectMethods/staticMethod.js
@@ -0,0 +1,20 @@
+function Article() {
+    this.created = new Date();
+    Article.lastCreated = Article.formatDate(this.created);
+    Article.count++;
+}
+
+Article.count = 0;
+
+Article.formatDate = function (date) {
+    return date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear();
+};
+
+Article.showStat = function () {
+    alert ('Всего: ' + Article.count + ', ' + 'последняя: ' + Article.lastCreated);
+};
+
+new Article();
+new Article();
+new Article();
+Article.showStat();
