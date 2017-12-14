cbfa41eb28daed91558b11c206b7f1658eafbd2e
Alexander Avramenko <alexander.avramenko@netent.com>
2017-12-14 12:19:16 +0200

    update

--- dots/dots.js
+++ dots/dots.js
@@ -1,12 +1,14 @@
-function getPoints(a, b, range, quantity) {
+var coordinates = [];
 
-    var x = b.x - a.x,
-        y = b.y - a.y,
+function getPoints(d1, d2, range, quantity) {
+
+    var x = d2.x - d1.x,
+        y = d2.y - d1.y,
         sectionX = x / quantity,
         sectionY = y / quantity,
-        pointX = a.x,
-        pointY = a.y,
-        coordinates = [];
+        pointX = d1.x,
+        pointY = d1.y,
+        dot = {};
         //randNum = Math.ceil(range * Math.random());
 
         for (var i = 0; i < quantity; i++) {
@@ -15,24 +17,47 @@ function getPoints(a, b, range, quantity) {
             coordinates.push({x: pointX, y: pointY})
         }
 
-
     console.log("x: " + x);
-    console.log("section: " + sectionX);
+    console.log("sectionX: " + sectionX);
+    console.log("sectionY: " + sectionY);
     console.log("y: " + y);
     console.log(coordinates);
 
+    // triangle
+    var A = Math.atan(sectionX / range),
+
+        a = range * Math.sin(A),
+        b = range * Math.cos(A);
+
+    console.log(Math.sin(A));
+    console.log(Math.cos(A));
+
+    console.log(a);
+    console.log(b);
+
+    if (y === 0) {
+        dot.x = coordinates[0].x;
+        dot.y = coordinates[0].y + range;
+    } else if (x === 0) {
+        dot.x = coordinates[0].x + range;
+        dot.y = coordinates[0].y;
+    } else {
+        dot.x = coordinates[0].x + b;
+        dot.y = coordinates[0].y + a;
+        }
+
+    console.log(dot);
+
 }
-
-
 
 var a1 = {
     x: 2,
-    y: 0
+    y: 3
 };
 
 var a2 = {
-    x: 14,
-    y: 0
+    x: 8,
+    y: 6
 };
 
-getPoints(a1, a2, 5, 4);
+getPoints(a1, a2, 10, 4);
