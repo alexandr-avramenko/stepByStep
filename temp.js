62ad9fd21f82f9815b0cbcfdbd364cedea736e1c
alexander.avramenko <avramenko423@gmail.com>
2018-04-03 10:14:16 +0300

    add new files

--- temp.js
+++ temp.js
@@ -1,1 +1,10 @@
-var canvas = document.getElementById('c1');
+var str = '{"title":"Конференция","date":"2014-11-30T12:00:00.000Z"}',
+    event = JSON.parse(str, function (key, value) {
+
+        if (key == 'date') {
+        return new Date(value);
+    }
+    return value;
+});
+
+console.log(event.date.getDate());
